97c7b4968e65749292cbf48f0e520fb1a3e91b5f
caoboidianguc <69663074+caoboidianguc@users.noreply.github.com>
2022-10-26 22:28:00 -0400

    Initial Commit

---
+++ DayEarn/DayEarnApp.swift
@@ -0,0 +1,17 @@
+//
+//  DayEarnApp.swift
+//  DayEarn
+//
+//  Created by Thong Vu on 10/26/22.
+//
+
+import SwiftUI
+
+@main
+struct DayEarnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
